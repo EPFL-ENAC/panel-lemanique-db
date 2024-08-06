3a3b1722fe231e8ace669526614bc4baf935e334
Hugo Solleder <hugo.solleder@epfl.ch>
2024-08-06 09:15:04 +0200

    feat: add Makefile rule to drop tables

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
-.PHONY: create_tables
+.PHONY: create_tables drop_tables
+
+drop_tables:
+	python3 panel_lemanique_db/drop_tables.py
 
 create_tables:
 	python3 panel_lemanique_db/create_tables.py
---
+++ panel_lemanique_db/drop_tables.py
@@ -0,0 +1,26 @@
+import os
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+from models.base import Base
+from models.participant_labels import ParticipantLabels  # noqa F401
+from models.participants import Participants  # noqa F401
+from models.question_labels import QuestionLabels  # noqa F401
+from models.questions import Questions  # noqa F401
+from models.responses import Responses  # noqa F401
+from models.sections import Sections  # noqa F401
+from models.surveys import Surveys  # noqa F401
+from models.survey_completion import SurveyCompletion  # noqa F401
+from models.survey_completion_labels import SurveyCompletionLabels  # noqa F401
+
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL, echo=True)
+Base.metadata.drop_all(engine)
