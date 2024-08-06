9dc7d0a689896663309012ba7722f8889ac1c138
Hugo Solleder <hugo.solleder@epfl.ch>
2024-08-06 09:11:05 +0200

    fix: prevent ruff from remove all models
    The ruff hook with pre-commit removed what was perceived to be unused
    imports, but SQLAlchemy needs these imports to create the tables.

--- panel_lemanique_db/create_tables.py
+++ panel_lemanique_db/create_tables.py
@@ -2,7 +2,16 @@ import os
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 
-from models.base import Base
+from models.base import Base  # noqa F401
+from models.participant_labels import ParticipantLabels  # noqa F401
+from models.participants import Participants  # noqa F401
+from models.question_labels import QuestionLabels  # noqa F401
+from models.questions import Questions  # noqa F401
+from models.responses import Responses  # noqa F401
+from models.sections import Sections  # noqa F401
+from models.surveys import Surveys  # noqa F401
+from models.survey_completion import SurveyCompletion  # noqa F401
+from models.survey_completion_labels import SurveyCompletionLabels  # noqa F401
 
 load_dotenv()
 
