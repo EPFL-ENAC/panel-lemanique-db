0277d2b4d5484299478feb991429f9011ccceb69
Hugo Solleder <hugo.solleder@epfl.ch>
2024-08-09 08:41:46 +0200

    feat: update wave2 preprocessing, util functions and documentation

--- data_preprocessing/preprocess_wave2.R
+++ data_preprocessing/preprocess_wave2.R
@@ -1,8 +1,21 @@
+# This script is used to preprocess the "raw" data from the second wave of the Lemanique Panel.
+# Running it will result in a collection of .tsv files that can be ingested in the PostgreSQL
+# database via the python ingestion script.
+
 here::i_am("./preprocess_wave2.R")
 
 source(here::here("utils.R"))
 
+# The "raw" data is stored on the LASUR drive. To be able to run the preprocessing, you will need
+# (at least) read access to the drive.
 folder <- "/Volumes/LASUR/common/LaSUR/06 - Recherche/Dossier de travail IT4R/"
+
+if (!dir.exists(folder)) {
+  stop(
+    sprintf("Error: dir '%s' is not accessible. ", folder),
+    "Read rights to the LASUR drive are necessary to preprocess the data. "
+  )
+}
 
 wave2_data <- haven::read_sav(
   file.path(
@@ -11,17 +24,34 @@ wave2_data <- haven::read_sav(
   )
 )
 
+# Most of the column names are messy, janitor helps clean them up automatically, forcing snake_case
+# column names that are also limited to ASCII characters. Column names included "Localité_source".
 names(wave2_data) <- janitor::make_clean_names(names(wave2_data), case = "snake", ascii = TRUE)
 
+# The names of four columns are slightly different in the database, so we change them here
+wave2_data <- wave2_data |>
+  dplyr::rename(
+    participant_code = idno,
+    group = groupe,
+    count_miss1 = countmiss1,
+    count_miss2 = countmiss2,
+    numero_insee = insee,
+    numero_ofs = ofs
+  )
+
+# Columns to be included in the participants file. This is currently more liberal than the table
+# definition
 participants_colnames <- c(
-  "idno", "pays", "groupe", "gp_age_source", "insee", "ofs", "weight", "titre_source", "cp_source",
-  "localite_source"
+  "participant_code", "pays", "group", "gp_age_source", "numero_insee", "numero_ofs", "weight",
+  "titre_source", "cp_source", "localite_source"
 )
 
+# Columns to be included in the survey_metadata (also named survey_completion) table
 survey_metadata_colnames <- c(
-  "countmiss1", "countmiss2", "progress", "start_date", "end_date", "temps_minute"
+  "count_miss1", "count_miss2", "progress", "start_date", "end_date", "temps_minute"
 )
 
+# These are additional column names that are currently simply discarded for the MVP
 # TODO: figure out with Panel team what to do with these additional variables.
 extra_colnames <- c(
   "titre_actuel", "cp_actuel", "localite_actuel", "code_raison_contact_1_v2",
@@ -30,7 +60,7 @@ extra_colnames <- c(
   "avant_chgmt_vet", "flag_chgmt_localite_v2"
 )
 
-# Clean-up questions
+# Prepare the questions and question_labels output
 questions <- wave2_data |>
   dplyr::select(
     -tidyselect::all_of(participants_colnames),
@@ -38,7 +68,9 @@ questions <- wave2_data |>
     -tidyselect::all_of(extra_colnames)
   )
 
-question_labels <- get_labels(questions, names_to = "question_code")
+question_labels <- get_labels(questions, names_to = "question_code") |>
+  dplyr::rename(label = name)
+question_labels <- remove_escapeseqs(question_labels)
 
 questions <- questions |>
   purrr::map(~ attr(.x, "label")) |>
@@ -60,22 +92,19 @@ questions <- questions |>
     "vot" ~ "Votation"
   ))
 
-# Need to create the survey and section IDs when these are added to the database, somehow
+# Some of the questions include escape characters (\n) that need to be removed
+questions <- remove_escapeseqs(questions)
+
+# Prepare the sections output
 sections <- questions |>
   dplyr::select(section_name) |>
   dplyr::group_by(section_name) |>
   dplyr::slice_head(n = 1) |>
   dplyr::ungroup()
 
-questions <- questions |>
-  dplyr::left_join(sections, by = "section_name") |>
-  dplyr::select(-section_name)
-
-# Clean-up participants
-
+# Prepare the participants and participant_labels output
 participants <- wave2_data |>
-  dplyr::select(tidyselect::all_of(participants_colnames)) |>
-  dplyr::rename(numero_insee = insee, numero_ofs = ofs)
+  dplyr::select(tidyselect::all_of(participants_colnames))
 
 participant_labels <- get_labels(participants)
 
@@ -83,16 +112,40 @@ participants <- participants |>
   zap_all() |>
   dplyr::mutate(numero_insee = strtoi(numero_insee, base = 10L))
 
-# Clean-up surveys
+# Prepare survey_completion data
 survey_completion <- wave2_data |>
   dplyr::select(
-    idno, countmiss1, countmiss2, progress, start_date, end_date, temps_minute, flag_troll
+    participant_code, count_miss1, count_miss2, progress, start_date, end_date, temps_minute,
+    flag_troll
   )
 
 survey_completion_labels <- get_labels(survey_completion)
 
 survey_completion <- survey_completion |>
   zap_all()
+
+# Prepare the responses table
+responses <- wave2_data |>
+  dplyr::select(
+    -all_of(c(participants_colnames[-1], survey_metadata_colnames, extra_colnames))
+  ) |>
+  zap_all()
+
+response_texts <- pivot_responses(
+  responses,
+  selection_type = "character", remove_NAs = TRUE, names_to = "question_code",
+  values_to = "response_text"
+) |>
+  remove_escapeseqs()
+
+response_values <- pivot_responses(
+  responses,
+  selection_type = "numeric", remove_NAs = TRUE, names_to = "question_code",
+  values_to = "response_value"
+)
+
+responses <- response_values |>
+  dplyr::bind_rows(response_texts)
 
 # Write everything to file
 output_folder <- "data/wave2"
@@ -112,3 +165,4 @@ readr::write_tsv(survey_completion, here::here(output_folder, "survey_completion
 readr::write_tsv(
   survey_completion_labels, here::here(output_folder, "survey_completion_labels.tsv")
 )
+readr::write_tsv(responses, here::here(output_folder, "responses.tsv"))
