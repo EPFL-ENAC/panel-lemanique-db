d030d866934b0ed3ce2f026579dc8074496beb3c
Hugo Solleder <hugo.solleder@epfl.ch>
2024-08-15 11:45:24 +0200

    feat: add /surveys/ and /sections/ path to API

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -1,7 +1,16 @@
 from sqlalchemy.orm import Session
 
 from ..database.models.surveys import Surveys
+from ..database.models.sections import Sections
 
 
 def get_survey(db: Session, survey_id: int):
     return db.query(Surveys).filter(Surveys.survey_id == survey_id).first()
+
+
+def get_surveys(db: Session, limit: int = 100):
+    return db.query(Surveys).limit(limit).all()
+
+
+def get_sections(db: Session, limit: int = 100):
+    return db.query(Sections).limit(limit).all()
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,7 @@
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
-
-from . import crud
 from backend.database.database import SessionLocal
+from . import crud
 
 app = FastAPI()
 
@@ -16,9 +15,21 @@ def get_db():
         db.close()
 
 
+@app.get("/surveys/")
+def read_surveys(limit: int = 100, db: Session = Depends(get_db)):
+    surveys = crud.get_surveys(db, limit=limit)
+    return surveys
+
+
 @app.get("/surveys/{survey_id}")
-def read_surveys(survey_id: int, db: Session = Depends(get_db)):
+def read_survey(survey_id: int, db: Session = Depends(get_db)):
     db_survey = crud.get_survey(db, survey_id=survey_id)
     if db_survey is None:
         raise HTTPException(status_code=404, detail="Survey not found")
     return db_survey
+
+
+@app.get("/sections/")
+def read_sections(limit: int = 100, db: Session = Depends(get_db)):
+    sections = crud.get_sections(db, limit=limit)
+    return sections
