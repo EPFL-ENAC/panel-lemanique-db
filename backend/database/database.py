589312a1f8ba13acbbc9bd8739eaefec8cf5a3b6
Hugo Solleder <hugo.solleder@epfl.ch>
2024-08-15 13:21:56 +0200

    refactor: remove declarative base from database
    The declarative base is declared in the base submodule

--- backend/database/database.py
+++ backend/database/database.py
@@ -1,7 +1,6 @@
 import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
@@ -16,5 +15,3 @@ DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NA
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
