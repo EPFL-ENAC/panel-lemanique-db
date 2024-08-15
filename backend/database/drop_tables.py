ba3838ba4f42d4e47348274b5e2b422218b7d126
Hugo Solleder <hugo.solleder@epfl.ch>
2024-08-15 11:05:54 +0200

    refactor: remove redundancy in engine creation

--- backend/database/drop_tables.py
+++ backend/database/drop_tables.py
@@ -1,6 +1,6 @@
-import os
-from sqlalchemy import create_engine
-from dotenv import load_dotenv
+from database import engine
+
+# noqa F401 is necessary to avoid having ruff automatically remove the unused imports
 from models.base import Base
 from models.participant_labels import ParticipantLabels  # noqa F401
 from models.participants import Participants  # noqa F401
@@ -12,15 +12,4 @@ from models.surveys import Surveys  # noqa F401
 from models.survey_completion import SurveyCompletion  # noqa F401
 from models.survey_completion_labels import SurveyCompletionLabels  # noqa F401
 
-load_dotenv()
-
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
-engine = create_engine(DATABASE_URL, echo=False)
 Base.metadata.drop_all(engine)
